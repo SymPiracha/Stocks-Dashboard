759f0fa73320f87621c7d31c61d48dacdc1b6a8e
Sym_Piracha <sym.piracha@gmail.com>
2021-01-27 22:52:50 -0500

    added reddit experiment script

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Stocks-Dashboard
+
+
+
+
+
+
+To get stock stock you need to use the iex finance API. You will need to download the package:
+
+pip install iexfinanc
---
+++ test_reddit_api.py
@@ -0,0 +1,26 @@
+import argparse
+import json
+import requests
+import time
+import datetime
+
+
+def main():
+
+    time_between_days_in_epoch = 86400
+    current_time = time.mktime(datetime.datetime.now().timetuple())
+    # comment
+	# print(data['data'][0]['body'])
+
+    request_data = requests.get('https://api.pushshift.io/reddit/search/submission/?q=BB&subreddit=stocks&after=1611619200&before=1611705600&size=1')
+    data = json.loads(request_data.content.decode('utf8'))
+    #print(data['data'][0]['created_utc'])
+
+
+    #mod_value = current_time%time_between_days_in_epoch
+
+    print(current_time)
+
+
+if __name__ == '__main__':
+	main()
