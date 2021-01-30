29c25a59f7362c0cd0b83354aebbdd0b3f8812fb
inavee <ibrahimnaveed@Ibrahims-MacBook-Air.local>
2021-01-29 22:30:44 -0500

    pushed the script to calculate the price change and the volume change

--- stock_db_test.py
+++ stock_db_test.py
@@ -1,14 +1,36 @@
 from iexfinance.stocks import get_historical_data
 from datetime import datetime
+from datetime import timedelta
 
 api_key = 'pk_495c80fadacc450e8d8912f83b9d4053'
 
-#stock = Stock('TSLA',token=api_key)
+today = datetime.now().strftime('%Y-%m-%d')
 
-start = datetime(2021, 1, 21)
-end = datetime(2021, 1, 29)
+#print(today)
 
-df = get_historical_data("TSLA", start, end,token=api_key)
+yesterday = (datetime.now() - timedelta(12)).strftime('%Y-%m-%d')
 
-print(df)
+df = get_historical_data("TSLA", yesterday, today,token=api_key)
 
+df = df .iloc[3:]
+
+df1 = df[['label','close','volume']]
+
+df1.columns = ['date','close','volume']
+
+price_change = []
+volume_change = []
+
+for i in range(5):
+    old_price = int(df1.iat[i,1])
+    new_price = int(df1.iat[i+1,1])
+    old_volume = int(df1.iat[i,2])
+    new_volume = int(df1.iat[i+1,2])
+    volume_change.append((new_volume-old_volume)/(old_volume)*100)
+    price_change.append((new_price-old_price)/(old_price) * 100)
+    
+df1 = df1.iloc[1:]
+df1['%volume_change'] = volume_change
+df1['%price_change'] = price_change
+
+print(df1)
